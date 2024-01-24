cead43c69a7f8917fa46472f5fd536cccade4959
Henadzi Rudchenko <grudchenko93@yandex.by>
2024-01-24 08:41:18 +0300

    feat: added clear directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,20 +4,38 @@ const path = require('path');
 const directoryPath = path.join(__dirname, 'files');
 const directoryPathCopy = path.join(__dirname, 'copy-files');
 
+const copyFiles = () => {
+  fs.readdir(directoryPath, (err, files) => {
+    files.forEach((file) => {
+      fs.copyFile(
+        `${directoryPath}/${file}`,
+        `${directoryPathCopy}/${file}`,
+        (err) => {
+          if (err) console.error(err);
+          console.log(`${file} скопирован`);
+        },
+      );
+    });
+  });
+};
+
+const clearDirectory = () => {
+  fs.readdir(directoryPathCopy, (err, files) => {
+    if (err) console.log(err);
+    files.forEach((file) => {
+      fs.unlink(path.join(directoryPathCopy, file), (err) => {
+        if (err) console.log(err);
+      });
+    });
+  });
+};
+
 fs.readdir(directoryPath, (err, files) => {
   fs.mkdir(directoryPathCopy, { recursive: true }, (err) => {
     if (err) console.error(err);
     console.log(`Директория ${directoryPathCopy} успешно создана`);
   });
 
-  files.forEach((file) => {
-    fs.copyFile(
-      `${directoryPath}/${file}`,
-      `${directoryPathCopy}/${file}`,
-      (err) => {
-        if (err) console.error(err);
-        console.log(`${file} скопирован`);
-      },
-    );
-  });
+  clearDirectory();
+  copyFiles();
 });
